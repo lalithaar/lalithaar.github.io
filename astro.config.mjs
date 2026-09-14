0a2c0a4f48dbf0dcf04a63b8769e3793c9a6e13f
Lalitha A R <lalithaar.research@gmail.com>
2026-09-14 14:49:38 +0530

    Remove remark-github-alerts plugin and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 // export default defineConfig({});
 
-import remarkGithubAlerts from 'remark-github-alerts';
 import { legacyRedirectsIntegration } from './scripts/legacy-redirects.mjs';
 
 export default defineConfig({
   site: 'https://isrl.in',
-  markdown: {
-    remarkPlugins: [remarkGithubAlerts],
-  },
   integrations: [legacyRedirectsIntegration()],
 });
