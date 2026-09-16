9c4de3fea620a12e914015d7d5577cc7cbf3612c
Lalitha A R <lalithaar.research@gmail.com>
2026-09-16 17:44:15 +0530

    weeding out not needed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 // export default defineConfig({});
@@ -8,5 +9,5 @@ import { legacyRedirectsIntegration } from './scripts/legacy-redirects.mjs';
 
 export default defineConfig({
   site: 'https://isrl.in',
-  integrations: [legacyRedirectsIntegration()],
+  integrations: [legacyRedirectsIntegration(), sitemap()],
 });
